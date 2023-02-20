af0279f35a096a176d7e7985c6fe54b9b3584404
Maneva <manevarsld@gmail.com>
2023-02-20 18:56:57 +0100

    update: app2,stubLockio

--- lockio-rasp/app2.py
+++ lockio-rasp/app2.py
@@ -3,14 +3,33 @@ from stubLockio import stubLockio
 app = Flask(__name__)
 
 lockio1 = stubLockio(1, "OCCUPIED")
-lockio2 = stubLockio(2, "OCCUPIED")
+lockio2 = stubLockio(2, "AVAILABLE")
 
 
-@app.route('/<int:id>/status')
+
+@app.route('/api/rasp/v1/lockio/<int:id>/test')
 def get_Lockio(id):
     return jsonify(vars(stubLockio.getLockio(id)))
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/api/rasp/v1/lockios')
+def get_Lockios():
+    return jsonify(vars(stubLockio.getListeLockio()));
+
+@app.route('/api/rasp/v1/lockio/<int:lockio_id>' , methods=['PATCH'])
+def patch_Lockio(lockio_id):
+    lockio = stubLockio.getLockio(lockio_id)
+    status = request.json['status']
+    if lockio.status == 'AVAILABLE':
+        lockio.status = status
+        return jsonify(lockio.serialize()), 200
+    else:
+        return '', 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, port=5000)
+
+
+
 
